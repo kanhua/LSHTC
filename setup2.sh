be8e46e0c111ee1d3c50742e28555e96d65136a0
kanhua <kanhwa@gmail.com>
2014-04-13 23:40:12 +0100

    update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,10 +1,11 @@
+mkdir source\ data
 
-wget https://www.dropbox.com/s/zf0y7v7tcfhgzdt/trainSparseMtx-min.p
-wget https://www.dropbox.com/s/aw2xw69w5v1937j/trainSparseMtx.p
-wget https://www.dropbox.com/s/xdb2boxa196wiom/y_train-min.p
-wget https://www.dropbox.com/s/3nmim6c3lc1x7fs/y_train.p
 wget https://www.dropbox.com/s/7lxk53jzlasu6d0/test-remapped.csv
 wget https://www.dropbox.com/s/rne4vv8qhc12ozs/test-remapped-min.csv
 wget https://www.dropbox.com/s/v87d2wkd1lhu9ny/train-sklearn.csv
 wget https://www.dropbox.com/s/4zt9an06dyxs8xk/test-sklearn.csv
 
+mv test-remapped.csv ./source\ data/
+mv test-remapped-min.csv ./source\ data/
+mv train-sklearn.csv ./source\ data/
+mv test-sklearn.csv ./source\ data/
