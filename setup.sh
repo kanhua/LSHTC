32231d3612c890acc16c8105cd5b7e711aa908bd
kanhua <kanhwa@gmail.com>
2014-04-13 22:53:09 +0100

    initial version

---
+++ setup.sh
@@ -0,0 +1,5 @@
+
+wget https://www.dropbox.com/s/zf0y7v7tcfhgzdt/trainSparseMtx-min.p
+wget https://www.dropbox.com/s/aw2xw69w5v1937j/trainSparseMtx.p
+wget https://www.dropbox.com/s/xdb2boxa196wiom/y_train-min.p
+wget https://www.dropbox.com/s/3nmim6c3lc1x7fs/y_train.p
