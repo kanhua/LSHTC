e1879f0657607c382bc1b7b8edea183b5a5bacfa
kanhua <kanhwa@gmail.com>
2014-04-18 11:50:27 +0100

    update the matrix calculation file

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include "omp.h"
 #include <ctype.h>
 
-#define NUM_THREADS 2
+#define NUM_THREADS 4
 
 #define Malloc(type,n) (type *)malloc((n)*sizeof(type))
 
 struct svm_parameter param;		// set by parse_command_line
-struct svm_problem prob;		// set by read_problem
+//struct svm_problem prob;		// set by read_problem
 struct svm_model *model;
 struct svm_node *x_space;
 
@@ -18,42 +18,67 @@ static char *line = NULL;
 static int max_line_len;
 
 
-void read_problem(const char *filename);
+void read_problem(const char *filename,struct svm_problem &prob);
 double spssum(struct svm_problem &prob,int row);
 void fillrowptr(struct svm_problem &prob1);
 double* spsmulti(struct svm_problem &prob1,struct svm_problem &prob2,double *resultmtx);
 
 int main(void)
 {
-	const char *testfile="train-sk-min.txt";
+	struct svm_problem train_prob;
+	struct svm_problem test_prob;
+
+
+	//char *datapath="C:\\Users\\Kan-Hua\\Dropbox\\Documents in Dropbox\\Programming projects\\Fun with Kaggle\\LSHTC\\source data\\";
 	
+	const char *datapath="./source data/";	
+	const char *trainminfile="train-sk-min.csv";
+	const char *testminfile="test-sk-min.csv";;
+	const char *testfile2="C:\\Users\\Kan-Hua\\Dropbox\\Documents in Dropbox\\Programming projects\\Fun with Kaggle\\LSHTC\\source data\\train-sklearn.csv";
+	
+	char *fullname=Malloc(char,strlen(datapath)+20);
+	strcpy(fullname,datapath);
+	strcat(fullname,trainminfile);
+	read_problem(fullname,train_prob);
+	fillrowptr(train_prob);
+	
+	strcpy(fullname,datapath);
+	strcat(fullname,testminfile);
+	read_problem(fullname,test_prob);
+	free(fullname);
+
+	fillrowptr(test_prob);
 	//read_problem("a1adata.dat");
-	read_problem(testfile);
+	//read_problem(testfile);
 	//read_problem("simplemtx.txt");
 	printf("file read!\n");
 	
 
-	fillrowptr(prob);
 	printf("%f\n",omp_get_wtime());
 	double *result=new double[1];
-	result=spsmulti(prob,prob,result);
+	result=spsmulti(test_prob,train_prob,result);
 	printf("%f\n",omp_get_wtime());
 
 	//print the result
-	FILE *ofp = fopen("output.dat", "w");
-
-	for (int i=0;i<prob.l;i++)
+	int printfile=0;
+	if (printfile)
 	{
-		for(int j=0;j<prob.l;j++)
-		{
-			fprintf(ofp,"%3.0f",result[i*prob.l+j]);
-			if (j<prob.l-1)
-				fprintf(ofp,",");
-			
-		}
-		fprintf(ofp,"\n");
-	}
+		FILE *ofp = fopen("output.dat", "w");
 
+		for (int i=0;i<train_prob.l;i++)
+		{
+			for(int j=0;j<test_prob.l;j++)
+			{
+				fprintf(ofp,"%3.0f",result[i*train_prob.l+j]);
+				if (j<train_prob.l-1)
+					fprintf(ofp,",");
+			
+			}
+			fprintf(ofp,"\n");
+		}
+
+		fclose(ofp);
+	}
 	int d=0;
 	scanf("?",&d);
 }
@@ -78,18 +103,41 @@ double *spsmulti(struct svm_problem &prob1,struct svm_problem &prob2,double *res
 	// determine the dimension of the matrix
 	
 	// Allocate the space for matrix
-	resultmtx=new double[prob1.l*prob2.l];
+	//resultmtx=new double[prob1.l*prob2.l];
+	
+	resultmtx=new double[1];
+
+	int totalelements=0;
+	
 	// check the dimension(optional for now)
 	//matrix multiplication
+
+	int totalcount=0;
+
+	//open the file
+	char *output_file_name=Malloc(char,10);
+	FILE *fp[NUM_THREADS];
+	for (int m=0;m<NUM_THREADS;m++)
+	{
+		sprintf(output_file_name,"outputfile_%d.txt",m);
+		fp[m]=fopen(output_file_name,"w");
+	}
+	
 
 	#pragma omp parallel for
 	for(int i=0;i<prob1.l;i++)
 	{
+		int threadnum=omp_get_thread_num();
+
+		//Allocate the momory for storing the result
+		struct svm_node *node_arr=Malloc(struct svm_node,prob2.l);
+		int nonzero_count=0;
+
 		for (int j=0;j<prob2.l;j++)
 		{
 			//calcualte the inner product of prob1[i,:] and prob2[j,:]
 			//run through the element in each row
-			
+
 			double sum=0;
 			for (int k=0;k<prob1.rowptr[i]-1;k++)
 			{
@@ -102,9 +150,32 @@ double *spsmulti(struct svm_problem &prob1,struct svm_problem &prob2,double *res
 				}
 			}
 		
-			resultmtx[i*prob1.l+j]=sum;
-			
+			//resultmtx[i*prob1.l+j]=sum;
+			if (sum>0)
+			{
+				node_arr[nonzero_count].index=j;
+				node_arr[nonzero_count].value=sum;
+				nonzero_count++;
+				totalelements++;
+			}
 		}
+	
+		//Write the result of each row into the file
+		fprintf(fp[threadnum],"%d ",i);
+		for(int p=0;p<nonzero_count;p++)
+		{
+			fprintf(fp[threadnum],"%d:%.1f ",node_arr[p].index,node_arr[p].value);
+		}
+		
+		fprintf(fp[threadnum],"\n");
+		free(node_arr);
+		//fprintf(fp[threadnum],"%d lines has been done,%d\n",i,totalelements);
+		
+	}
+	printf("total elements are %d\n",totalelements);
+	for (int m=0;m<NUM_THREADS;m++)
+	{
+		fclose(fp[m]);
 	}
 
 	return resultmtx;
@@ -112,14 +183,14 @@ double *spsmulti(struct svm_problem &prob1,struct svm_problem &prob2,double *res
 
 void fillrowptr(struct svm_problem &prob1)
 {
-	prob1.rowptr=Malloc(int,prob.l);
-	for(int i=0;i<prob.l;i++)
+	prob1.rowptr=Malloc(int,prob1.l);
+	for(int i=0;i<prob1.l;i++)
 	{
 		int index=-1;
 		int cols=0;
 		do
 		{
-			index=prob.x[i][cols].index;
+			index=prob1.x[i][cols].index;
 			cols++;
 		}while(index!=-1);
 		
@@ -153,7 +224,7 @@ void exit_input_error(int line_num)
 	exit(1);
 }
 
-void read_problem(const char *filename)
+void read_problem(const char *filename,struct svm_problem &prob)
 {
 
 	int elements, max_index, inst_max_index, i, j;
@@ -161,6 +232,9 @@ void read_problem(const char *filename)
 	char *endptr;
 	char *idx, *val, *label;
 	int errno;
+
+	char *leftline=NULL;
+	
 
 	if(fp == NULL)
 	{
@@ -202,16 +276,19 @@ void read_problem(const char *filename)
 	{
 		inst_max_index = -1; // strtol gives 0 if wrong format, and precomputed kernel has <index> start from 0
 		
-		
+		//printf("reading line %d\n",i);
+
 		readline(fp); // read the next line in the file into global "line" variable
 		
 		prob.x[i] = &x_space[j];
 		
 		//below is the procedure that extracts the mulilabel(add by KH)
 		
+
 		label = strtok(line,",");
-		char *leftline=Malloc(char,max_line_len);
-		leftline=strcpy(leftline,line);
+
+		leftline=Malloc(char,max_line_len);
+		strcpy(leftline,line);
 		while(1)
 		{
 			label=strtok(NULL,",");
@@ -255,6 +332,7 @@ void read_problem(const char *filename)
 		if(inst_max_index > max_index)
 			max_index = inst_max_index;
 		x_space[j++].index = -1;
+		free(leftline);
 	}
 
 	if(param.gamma == 0 && max_index > 0)
